a27869f0f939d6e12d643f2429ecd82f0523a536
vincent-guizot <vincentguizot128@gmail.com>
2026-01-26 14:57:37 +0700

    material

---
+++ server/src/models/material.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Material extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Material.init({
+    classId: DataTypes.INTEGER,
+    meetingId: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    type: DataTypes.STRING,
+    url: DataTypes.STRING,
+    uploadedBy: DataTypes.INTEGER,
+    imageUrl: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Material',
+  });
+  return Material;
+};
