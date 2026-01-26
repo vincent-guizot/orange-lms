72dff3243f758c614de3dcc010588f916b9cf8ee
vincent-guizot <vincentguizot128@gmail.com>
2026-01-26 14:48:04 +0700

    meeting

---
+++ server/src/models/meeting.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Meeting extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Meeting.init({
+    classId: DataTypes.INTEGER,
+    meetingNumber: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    meetingDate: DataTypes.DATE,
+    startHour: DataTypes.TIME,
+    finishHour: DataTypes.TIME,
+    imageUrl: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Meeting',
+  });
+  return Meeting;
+};
