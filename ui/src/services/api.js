86699d8256def5185b4bde168ea4797efc7e502b
vincent-guizot <vincentguizot128@gmail.com>
2026-01-30 22:06:10 +0700

    note page

--- ui/src/services/api.js
+++ ui/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // backend base URL
+  baseURL: "http://localhost:5000", // backend base URL
   headers: {
     "Content-Type": "application/json",
   },
