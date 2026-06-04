e6db0cd8b87dede161023eff218f0b8fe58c4092
vincent-guizot <vincentguizot128@gmail.com>
2026-06-04 23:03:38 +0700

    refactoring back end

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -16,6 +16,5 @@ router.delete("/:id", UserController.delete);
  * User Profile
  */
 router.get("/:id/profile", ProfileController.getByUser);
-router.post("/:id/profile", ProfileController.createOrUpdate);
-
+router.put("/:id/profile", ProfileController.update);
 module.exports = router;
