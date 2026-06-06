53cc6a1506a9206c386c058b606e876ee8ab7201
vincent-guizot <vincentguizot128@gmail.com>
2026-06-07 00:30:44 +0700

    refactoring app, store, theme providers

--- ui/src/app/App.jsx
+++ ui/src/app/App.jsx
@@ -1,14 +1,17 @@
-import React from "react";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
 import store from "./store";
 import router from "./routes";
 
+import PopupProvider from "./providers/PopupProvider";
+
 function App() {
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
+
+      <PopupProvider />
     </Provider>
   );
 }
